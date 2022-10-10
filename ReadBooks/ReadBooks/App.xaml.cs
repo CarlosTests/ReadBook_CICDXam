711fca25140c1e96aedb418d46d66abcac9cdf5c
actcarlossilva <carlos.silva@activetech.pt>
2022-10-10 14:52:53 +0100

    Add AppCenter Crashes & Analytics services

--- ReadBooks/ReadBooks/App.xaml.cs
+++ ReadBooks/ReadBooks/App.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +18,9 @@ namespace ReadBooks
 
         protected override void OnStart ()
         {
+            AppCenter.Start("ios=c821e1d7-4693-4b23-ab7f-43bc6bd4fd36;"+
+                            "android=089323c2-51be-4c53-9b79-d336d33754f8",
+                            typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep ()
